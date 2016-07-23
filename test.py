ee2db81780fbf785e99dbf1c2a35a0ca4fd26fe3
urban <anatoly.urban@gmail.com>
2016-07-23 19:59:08 +0300

    Deleted spare file.

--- test.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'urban'
-from Bio import SearchIO
-
-qrs = SearchIO.parse("tmp_blast/3.xml", "blast-xml")
-for qr in qrs:
-    for hit in qr:
-        for hsp in hit:
-            print(hsp)
