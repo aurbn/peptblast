5f3aebe0c492548054c401dcc43109df32f96ecf
aurbn <anatoly.urban@gmail.com>
2016-07-30 02:49:34 +0300

    Added gi annnotation script.

--- gi_annotator.py
+++ gi_annotator.py
@@ -2,18 +2,24 @@
 import sys
 import os
 import tempfile
+import shutil
 from argparse import ArgumentParser
+from Bio import Entrez
 
 def main():
     argparser = ArgumentParser(description="Add gi annotation to peptblast output")
     argparser.add_argument("infile", type = str,  help = "Peptblast output file.")
+    argparser.add_argument('--nobk', action='store_true', required=False, help='Do not save backup file.')
     argparser = argparser.parse_args()
 
     outfile = tempfile.NamedTemporaryFile(delete=False)
     outfilename = outfile.name
 
+    Entrez.email = "anatoly.urba[at]gmail.com"
+
     gis = []
 
+    print("Scanning input.")
     with open(argparser.infile) as f:
         for line in f.readlines():
             if line.startswith("gi|"):
@@ -22,13 +28,36 @@ def main():
                     gi = gi.split('|')[1]
                     gis.append(gi)
 
-    print gis
 
+    ann = {}
+    print("Fetching data.")
+    data = Entrez.esummary(db="protein", id = ",".join(gis))
+    entrez = Entrez.read(data)
+    for e in entrez:
+        ann[e['Gi']] = e['Title']
+    print("%i ids fetched" % len(ann))
 
-    print outfilename
+    print("Addinng annotation.")
+    with open(argparser.infile) as f:
+        for line in f.readlines():
+            if line.startswith("gi|"):
+                gi, _t, descr = line.split('\t')
+                if descr.strip() == "":
+                    gi_ = gi.split('|')[1]
+                    descr = ann.get(int(gi_), "")
+                line = '\t'.join([gi, _t, descr, '\n'])
+            outfile.writelines(line)
 
+    outfile.close()
 
+    print("Done.")
+    if argparser.nobk:
+        os.unlink(argparser.infile)
+    else:
+        print("Backup file saved.")
+        os.rename(argparser.infile, argparser.infile+".bk")
 
+    shutil.move(outfilename, argparser.infile)
 
 
 if __name__ == '__main__':
