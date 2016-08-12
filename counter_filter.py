484a2c6d00be7b39887f3f5d51d0079eaf0a0401
urban <anatoly.urban@gmail.com>
2016-08-12 17:29:21 +0300

    Added orgamisms filter through Entrez.

--- counter_filter.py
+++ counter_filter.py
@@ -6,6 +6,13 @@ from argparse import ArgumentParser
 from collections import defaultdict
 from shutil import copy
 from os import unlink
+from Bio import Entrez
+from time import sleep
+Entrez.email = "anatoly.urban@gmail.com"
+try:
+    from tqdm import tqdm
+except:
+    tqdm = lambda x: x
 
 
 def grouper(n, iterable):
@@ -22,10 +29,12 @@ def main():
     argparser.add_argument("infile", type = str,  help = "Peptblast output file.")
     argparser.add_argument("--count", type = str, choices = ["query", "hit"], default="query",
                            help = "Id to count")
-    argparser.add_argument("--nosort", action="store_true", help = "Doesnt sort an d modify input file.")
+    argparser.add_argument("--nosort", action="store_true", help = "Doesnt sort and modify input file.")
     argparser.add_argument("--table", type=str, required=False, default="table.txt",
                            help="Output table of occurrences.")
     argparser.add_argument('--nobk', action='store_true', required=False, help='Do not save backup file.')
+    argparser.add_argument("--taxfilter", type=str, required=False,
+                           help="Annlow only hits with these taxons")
     argparser.add_argument("--organisms", type=str, required=False,
                            help="Output table for organism frequencies.")
     argparser.add_argument("--le", type=int, required=False,
@@ -44,7 +53,7 @@ def main():
             id_ = ch[5].split('\t')[0]
         else:
             exit(1)
-        if argparser.organisms:
+        if argparser.organisms or argparser.taxfilter:
             org = ch[5].split('\t')[2]
             if '[' in org:
                 org = org.split('[')[1]
@@ -59,6 +68,60 @@ def main():
 
     freqs = freqs.items()
     freqs.sort(key=itemgetter(1))
+
+    okorgs = []  #filtered organisms
+    if argparser.taxfilter:
+        taxfilters = {f for f in argparser.taxfilter.lower().split(',')}
+        orgs = orgfreqs.keys()
+        print("Fetching annotations...")
+        for org in tqdm(orgs):
+            attempts = 10
+            while attempts:
+                try:
+                    data = Entrez.esearch(db="taxonomy", term = org)
+                    ret = Entrez.read(data)
+                    break
+                except RuntimeError:
+                    attempts -= 1
+                    sleep(1)
+            else:
+                print("Failed fetch annotation for %s" % org)
+
+            if int(ret["Count"]) == 1:
+                data = Entrez.efetch(db = "taxonomy", id = ret["IdList"][0])
+                ret = Entrez.read(data)[0]
+                lineage = ret["Lineage"]
+                lineage = {l.lower().strip() for l in lineage.split(';')}
+                if taxfilters.intersection(lineage):
+                    okorgs.append(org)
+
+            elif int(ret["Count"]) > 1:
+                print("Muliple search results for organism %s, leaving" % org)
+                okorgs.append(org)
+            else:
+                print("No search results for organism %s, leaving" % org)
+                okorgs.append(org)
+
+        ## Update all freqencies
+        orgfreqs = defaultdict(int)
+        freqs = defaultdict(int)
+        descrs = {}
+        chunks = defaultdict(list)
+        for ch in grouper(7, open(argparser.infile).readlines()):
+            org = ch[5].split('\t')[2]
+            if '[' in org:
+                org = org.split('[')[1]
+                org = org.replace(']', "")
+                org = org.strip()
+                if org in okorgs:
+                    orgfreqs[org] += 1
+                    if argparser.count == "query":
+                        id_ = ch[1].split('\t')[0]
+                    elif argparser.count == "hit":
+                        id_ = ch[5].split('\t')[0]
+                    else:
+                        exit(1)
+
 
     if argparser.table:
         with open(argparser.table, "w") as tab:
